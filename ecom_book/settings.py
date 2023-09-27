fe131d2965e4bc5ed4ef893ca230163f44cf1165
Musthakeem <>
2023-09-27 10:49:21 +0530

    git ignore updated by removing static files

--- ecom_book/settings.py
+++ ecom_book/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wh-u035d25ph46&6=4(%ynp)=yss$9_l349@*9crxw^!r%v++='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -126,6 +126,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static')
 ]
+STATIC_ROOT = BASE_DIR/'static'
+
+MEDIA_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
